365692b66e5eb8cbbbc4b73eee7862beb809c297
Tomek20225 <tomek202255@gmail.com>
2023-04-14 23:49:36 +0200

    Added plane, shadows and camera controls

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,32 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Renderer (canvas) configuration
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setPixelRatio( window.devicePixelRatio );
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild( renderer.domElement );
 
-// Setup the scene and the camera
+// Scene setup
 const scene = new THREE.Scene();
 
-// Adding lightning
-scene.add(new THREE.HemisphereLight(0xffffff,0xffffff,1.0))
-
-// Setup the map
+// Map setup
 const map = [
+    [1],
     [1, 1, 1, 1, 1],
     [1, 0, 0, 0, 1],
     [1, 0, 0, 0, 1],
     [1, 1, 1, 1, 1],
 ];
-const sizeMultiplier = 1;
-let mapLongestSide = 0;
+const mapLongestSide = map.reduce((acc, currentValue) => currentValue.length > acc ? currentValue.length : acc, 0);
 
 for (let y = 0; y < map.length; y++) {
     let lengthHolder = 0;
     let startingX = -1;
 
-    mapLongestSide = (map[y].length > mapLongestSide) ? map[y].length : mapLongestSide;
+    //mapLongestSide = (map[y].length > mapLongestSide) ? map[y].length : mapLongestSide;
 
     for (let x = 0; x < map[y].length; x++) {
         if (map[y][x] == 1) {
@@ -39,25 +39,51 @@ for (let y = 0; y < map.length; y++) {
 
         const wallGeometry = new THREE.BoxGeometry( lengthHolder, 1, 2 );
         wallGeometry.translate( lengthHolder / 2, 1 / 2, 1 / 2 );
-        const wallMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        const wallMaterial = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
         const wall = new THREE.Mesh( wallGeometry, wallMaterial );
 
+        wall.castShadow = true;
+        wall.receiveShadow = true;
+
         scene.add( wall );
-        wall.position.set(startingX * sizeMultiplier, -y * sizeMultiplier, 0);
+        wall.position.set(startingX, -y, 0);
 
         lengthHolder = 0;
         startingX = -1;
     }
 }
 
+const mapCenterPosition = new THREE.Vector2((mapLongestSide - 1) / 2, -((map.length - 1) / 2));
 
-// Setup the camera
+// Plane setup
+const planeGeometry = new THREE.PlaneGeometry(4, 4);
+planeGeometry.translate(4 / 2, -4 / 2, 0); // TODO: Wrong position in the upper left corner
+const planeMaterial = new THREE.MeshPhongMaterial({color: 0xffffff});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.castShadow = false;
+plane.receiveShadow = true;
+scene.add(plane)
+
+// Camera setup
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set((mapLongestSide - 1) / 2, -((map.length - 1) / 2), 50);
+camera.position.set(mapCenterPosition.x, mapCenterPosition.y, 10);
+
+// Light setup
+const light = new THREE.DirectionalLight(0xffffff, 1.0);
+light.position.set(mapCenterPosition.x, mapCenterPosition.y, 90);
+light.target.position.set(0, 0, 0);
+light.castShadow = true;
+scene.add(light);
+scene.add( new THREE.CameraHelper( light.shadow.camera ) );
+
+// Camera controls setup
+//const controls = new OrbitControls(camera, renderer.domElement);
+
 
 // The main loop
 function animate() {
 	requestAnimationFrame( animate );
+    //controls.update();
 	render();
 }
 
