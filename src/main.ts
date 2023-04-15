5c5fc5741c9fd362f8e3a4d8767733c15737d867
Tomek20225 <tomek202255@gmail.com>
2023-04-15 20:14:11 +0200

    Added collision detection and player acceleration

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,9 @@ const scene = new THREE.Scene();
 const map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
@@ -37,6 +38,12 @@ const mapWalkablePositions = map.reduce((acc, currentValue, index) => {
     return Array.prototype.concat(acc, walkablePositionsInRow);
 }, [] as THREE.Vector2[]);
 const getRandomWalkablePosition = () => mapWalkablePositions[(Math.floor(Math.random() * mapWalkablePositions.length))];
+
+type Wall = {
+    instance: THREE.Mesh,
+    box: THREE.Box3
+}
+const walls = [] as Wall[];
 
 for (let y = 0; y < mapHeight; y++) {
     let lengthHolder = 0;
@@ -62,6 +69,10 @@ for (let y = 0; y < mapHeight; y++) {
         wall.receiveShadow = true;
 
         scene.add( wall );
+        walls.push({
+            instance: wall,
+            box: new THREE.Box3().setFromObject(wall)
+        } as Wall);
 
         lengthHolder = 0;
         startingX = -1;
@@ -100,19 +111,22 @@ scene.add(light.target);
 // scene.add( new THREE.CameraHelper( light.shadow.camera ) );
 
 // Player setup
-const playerConfig = {
-    geometry: new THREE.SphereGeometry(0.35),
-    material: new THREE.MeshPhongMaterial({color: 0x0000ff}),
-    size: 0.35
-};
 const playerRandomPos = getRandomWalkablePosition();
+const playerSize = 0.35;
+const playerConfig = {
+    geometry: new THREE.SphereGeometry(playerSize).translate(playerSize, playerSize, 0),
+    material: new THREE.MeshPhongMaterial({color: 0x0000ff}),
+    size: playerSize,
+    speed: 0.05,
+    accRate: 0.001
+};
 const player = {
     id: 1,
-    position: new THREE.Vector3(playerRandomPos.x, playerRandomPos.y, playerConfig.size),
-    instance: new THREE.Mesh(playerConfig.geometry, playerConfig.material)
+    i: new THREE.Mesh(playerConfig.geometry, playerConfig.material),
+    acc: 0
 };
-player.instance.position.set(player.position.x, player.position.y, player.position.z);
-scene.add(player.instance);
+player.i.position.set(playerRandomPos.x, playerRandomPos.y, playerConfig.size);
+scene.add(player.i);
 
 // Camera controls setup
 // const controls = new OrbitControls(camera, renderer.domElement);
@@ -133,25 +147,25 @@ const keyStates = {
     mouseDir: new THREE.Vector2()
 }
 
-function onKeyDown(e: KeyboardEvent) {
+function onKeyDown(e: KeyboardEvent):void {
     e.preventDefault();
     if (!e.key.includes("Arrow")) return;
     keyStates[e.key.replace("Arrow", "").toLowerCase()] = true;
 }
 
-function onKeyUp(e: KeyboardEvent) {
+function onKeyUp(e: KeyboardEvent): void {
     e.preventDefault();
     if (!e.key.includes("Arrow")) return;
     keyStates[e.key.replace("Arrow", "").toLowerCase()] = false;
 }
 
-function onMouseDown(e: MouseEvent) {
+function onMouseDown(e: MouseEvent): void {
     e.preventDefault();
     if (e.button != 0) return;
     keyStates.mouse = true;
 }
 
-function onMouseUp(e: MouseEvent) {
+function onMouseUp(e: MouseEvent): void {
     e.preventDefault();
     if (e.button != 0) return;
 
@@ -162,13 +176,13 @@ function onMouseUp(e: MouseEvent) {
     keyStates.mouse = false;
 }
 
-function onMouseMove(e: MouseEvent) {
+function onMouseMove(e: MouseEvent): void {
     e.preventDefault();
     const mouseX = (e.clientX / WIDTH) * 2 - 1;
     const mouseY = -(e.clientY / HEIGHT) * 2 + 1;
 
     const playerPosition = new THREE.Vector3();
-    playerPosition.copy(player.position);
+    playerPosition.copy(player.i.position);
     playerPosition.project(camera);
 
     const areaError = 0.025;
@@ -180,23 +194,65 @@ function onMouseMove(e: MouseEvent) {
         keyStates.mouseDir.set(mouseX - playerPosition.x, mouseY - playerPosition.y);
 }
 
-function updatePlayerPosition() {
-    const speed = 0.05;
+// Extrapolation of player position
+function getPlayerFuturePosition(): THREE.Vector3 {
+    const speed = playerConfig.speed + player.acc;
+    const futurePlayerPos = new THREE.Vector3().copy(player.i.position);
 
     if (keyStates.mouse) {
-        player.position.y += keyStates.mouseDir.y * speed;
-        player.position.x += keyStates.mouseDir.x * speed;
+        futurePlayerPos.y += keyStates.mouseDir.y * speed;
+        futurePlayerPos.x += keyStates.mouseDir.x * speed;
     }
     else {
-        if (keyStates.up) player.position.y += speed;
-        if (keyStates.down) player.position.y -= speed;
-        if (keyStates.left) player.position.x -= speed;
-        if (keyStates.right) player.position.x += speed;
+        if (keyStates.up) futurePlayerPos.y += speed;
+        if (keyStates.down) futurePlayerPos.y -= speed;
+        if (keyStates.left) futurePlayerPos.x -= speed;
+        if (keyStates.right) futurePlayerPos.x += speed;
     }
 
-    player.instance.position.copy(player.position);
+    return futurePlayerPos;
 }
 
+// Updating player position
+function updatePlayerPosition(futurePos: THREE.Vector3): void {
+    player.i.position.copy(futurePos);
+}
+
+// Detecting whether the player is currently moving
+// If player is colliding with a wall, then he is not moving
+function playerIsMoving(isColliding: boolean): boolean {
+    if (!isColliding && (keyStates.up || keyStates.down || keyStates.left || keyStates.right || keyStates.mouse))
+        return true;
+
+    return false;
+}
+
+// Updating player acceleration based on whether he is moving
+function updatePlayerAcceleration(isColliding: boolean): void {
+    if (isColliding) {
+        player.acc = 0;
+        return;
+    }
+
+    if (playerIsMoving(isColliding)) player.acc += playerConfig.accRate;
+    else player.acc = Math.max(player.acc - playerConfig.accRate, 0);
+}
+
+// Collision detection
+function detectCollisions(playerFuturePos: THREE.Vector3): boolean {
+    const futurePlayer = new THREE.Mesh(playerConfig.geometry, playerConfig.material);
+    futurePlayer.position.copy(playerFuturePos);
+
+    const playerBox = new THREE.Box3().setFromObject(futurePlayer);
+
+    for (const wall of walls) {
+        if (wall.box.intersectsBox(playerBox)) {
+            return true;
+        }
+    };
+
+    return false;
+}
 
 // The main loop
 function animate() {
@@ -204,7 +260,12 @@ function animate() {
 
     // controls.update();
 
-    updatePlayerPosition();
+    const playerFuturePosition = getPlayerFuturePosition();
+    const isColliding = detectCollisions(playerFuturePosition);
+    if (!isColliding)
+        updatePlayerPosition(playerFuturePosition);
+
+    updatePlayerAcceleration(isColliding);
 
     render();
 }
